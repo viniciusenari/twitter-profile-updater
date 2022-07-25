32704770a1a492adf3dfc7c20f87312175696fb9
Vinicius Koji Enari <viniciusenari@gmail.com>
2022-07-25 14:54:59 -0300

    feat: API keys, access tokens, OAuth
    Get API key, API secret, access token, and access token secret from config file. Authenticate API

---
+++ app.py
@@ -0,0 +1,15 @@
+import tweepy
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+api_key = config['twitter']['api_key']
+api_key_secret = config['twitter']['api_key_secret']
+access_token = config['twitter']['access_token']
+access_token_secret = config['twitter']['access_token_secret']
+
+auth = tweepy.OAuthHandler(api_key, api_key_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
